855d3a4f3442c5730e406b7991da008e2f6e164c
kangyul <39001942+kangyul@users.noreply.github.com>
2021-06-08 19:12:37 +0900

    Create 5.BubbleSorting.c

---
+++ chp15_Prac_Programming2/5.BubbleSorting.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void BubbleSort(int ary[], int len);
+
+int main(void) {
+	int arr[7];
+	int i;
+	
+	printf("Bubble Sorting Program\n"); 
+	
+	for(i=0; i<7; i++) {
+		printf("입력: ");
+		scanf("%d", &arr[i]);
+	}
+	
+	BubbleSort(arr, sizeof(arr)/sizeof(int));
+	for(i=0; i<7; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");	
+	return 0;
+}
+
+void BubbleSort(int ary[], int len)
+{
+	int i,j;
+	int temp;
+	
+	for(i=0; i<len-1; i++) {
+		for(j=0; j<(len-i)-1; j++) {
+			if(ary[j] < ary[j+1]) {
+				temp = ary[j];
+				ary[j] = ary[j+1];
+				ary[j+1] = temp;
+			}
+		}
+	}
+}
